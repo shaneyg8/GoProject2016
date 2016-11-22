0c7d6b2b69bc72797b5351f24c28d4b438778060
shaneyg8 <shanegleeson22@gmail.com>
2016-11-22 18:10:27 +0000

    3rd commit

--- Go Project 2016/webapp.go
+++ Go Project 2016/webapp.go
@@ -3,31 +3,47 @@ package main
 import (
 	"gopkg.in/macaron.v1"
 	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 	"net/http"
-	//"fmt"
+	"fmt"
 	
 	
 )
-
+type List struct {
+ 	ID       bson.ObjectId `bson:"_id,omitempty"`
+ 	Date    string
+    ToDo    string
+ }
 func main() {
 	m := macaron.Classic()
 	m.Use(macaron.Renderer())
 	m.Post("/", save)
-	m.Get("/search/:id", func(ctx *macaron.Context) {
-		// Adapted from: https://go-macaron.com/docs/middlewares/templating
-		//ctx.Data["id"] = search(ctx.Params(":id"))
-		//ctx.HTML(200, "hello")
-	})
 	m.Run(4000)
 }
 
 func save(req *http.Request) {
 	//fmt.Println("Uploadhandler start")
-	session, err := mgo.Dial("127.0.0.1:50864")
+	session, err := mgo.Dial("127.0.0.1:27017")
 	if err != nil {
 		panic(err)
 	}
 
 	defer session.Close()
+    req.ParseForm()
+    date := req.FormValue("date")
+    todo := req.FormValue("todo")
+	//Check if there is an error
+	if err != nil {
+		fmt.Println(err)
+	}
+    
+    // Collection
+ 	c := session.DB("Todo").C("List")
+ 
+ 	// Insert
+ 	err = c.Insert(&List{Date: date, ToDo: todo})
+    if err != nil {
+        panic(err)
+    }
 	
 }
