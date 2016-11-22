476f2f3762fea530b8aea99060eabfc9d9e972a9
shaneyg8 <shanegleeson22@gmail.com>
2016-11-22 16:22:49 +0000

    2nd Commit

--- Go Project 2016/webapp.go
+++ Go Project 2016/webapp.go
@@ -1,20 +1,33 @@
-
-// Adapted from: https://go-macaron.com
-
 package main
 
-import "gopkg.in/macaron.v1"
-
-// Function taken from: github.com/golang/example
-
+import (
+	"gopkg.in/macaron.v1"
+	"gopkg.in/mgo.v2"
+	"net/http"
+	//"fmt"
+	
+	
+)
 
 func main() {
-  m := macaron.Classic()
-  m.Use(macaron.Renderer())
-  
-  m.Get("/macaron", func() string {
-    return "Hello from Macaron!"
-  })
-  
-  m.Run(8080)
+	m := macaron.Classic()
+	m.Use(macaron.Renderer())
+	m.Post("/", save)
+	m.Get("/search/:id", func(ctx *macaron.Context) {
+		// Adapted from: https://go-macaron.com/docs/middlewares/templating
+		//ctx.Data["id"] = search(ctx.Params(":id"))
+		//ctx.HTML(200, "hello")
+	})
+	m.Run(4000)
+}
+
+func save(req *http.Request) {
+	//fmt.Println("Uploadhandler start")
+	session, err := mgo.Dial("127.0.0.1:50864")
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+	
 }
